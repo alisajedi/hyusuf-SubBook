a50915a5b9845c1b038de9917d7a748107fbca1a
Hamdi Yusuf <hyusuf63@gmail.com>
2018-01-14 00:01:03 -0700

    first commit

---
+++ app/src/main/java/com/example/hyusuf/subbook/CreateSubActivity.java
@@ -0,0 +1,17 @@
+package com.example.hyusuf.subbook;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by hyusuf on 2018-01-13.
+ */
+
+public class CreateSubActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sublayout);
+    }
+}
